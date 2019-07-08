a623086826249cb3ff1c38c38350c15d55ce0b40
nicsachin <nicsachin007@gmail.com>
2019-07-08 16:32:08 +0530

    done

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text , Button , View , Image , ScrollView} from 'react-native';
-import CameraRoll from "@react-native-community/cameraroll";
+import {Text, Button, View, Image, ScrollView} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
+import {Col, Row, Grid} from 'react-native-easy-grid';
+
 class App extends React.Component {
 
     constructor() {
@@ -12,42 +14,52 @@ class App extends React.Component {
 
     open() {
 
-        console.log('>>> inside open')
+        console.log('>>> inside open', this.state.photos);
         CameraRoll.getPhotos({
-            first: 20,
+            first: 100,
             assetType: 'Photos',
         })
             .then(r => {
-                this.setState({ photos: r.edges });
+                this.setState({photos: r.edges});
             })
             .catch((err) => {
                 //Error Loading Images
-                console.log(err)
+                console.log(err);
             });
 
     }
 
     render() {
 
-        return ( <View>
-            <Button title="Load Images" onPress={this.open} />
-            <ScrollView>
-                {this.state.photos.map((p, i) => {
-                    return (
-                        <Image
-                            key={i}
-                            style={{
-                                width: 500,
-                                height: 500,
-                                borderRadius:3,
-                                borderColor:"red"
-                            }}
-                            source={{ uri: p.node.image.uri }}
-                        />
-                    );
-                })}
-            </ScrollView>
-        </View>);
+        return (
+
+            <Grid>
+                <Row size={60} style={{backgroundColor: 'red'}}>
+                    <Image
+                        style={{  flex: 1,
+
+                            alignSelf: 'center',
+                            width: '100%',
+                            height: '100%'}}
+                        source={{uri: 'https://www.mini.in/en_IN/home/jcr:content/main/home_teaser_gallery/home-teaser-gallery-par/home_teaser_item_cop_783170173/image-item/damImage.wide.560w.j_1561456852353.jpg'}}
+                    />
+                </Row>
+                <Row size={20} style={{backgroundColor: 'green'}}>
+                    <Col style = {{backgroundColor: "red"}}></Col>
+                    <Col style = {{backgroundColor: "black"}}></Col>
+                    <Col></Col>
+                </Row>
+
+                <Row size={20} style={{backgroundColor: 'red'}}>
+                    <Col style = {{backgroundColor: "black"}}></Col>
+                    <Col style = {{backgroundColor: "red"}}></Col>
+                    <Col style = {{backgroundColor: "black"}}></Col>
+
+                </Row>
+            </Grid>
+
+
+        );
     }
 
 }
